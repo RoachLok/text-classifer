fe368e23a7f623f2d46427d55dd943820fadd5d6
RoachLok <jtabordajunior@hotmail.com>
2021-01-14 12:26:13 +0100

    Removed unnecessary imports and fixed ModeloDespo import.

--- conexions.py
+++ conexions.py
@@ -1,14 +1,9 @@
-import glob
-import chardet
 import os
 import pandas as pd
 from pathlib import Path
-from charset_normalizer import detect
-from chardet.universaldetector import UniversalDetector
-from IPython.display import display
 from tkinter import filedialog
 from tkinter import *
-from modelo_noticias_despoblacion import cargarTextosTest, cargarTextosTraining, load_model, model_testing, model_training, save_model
+from modelo_noticias_despoblacion import ModeloDesp
 
 
 class ModeloDesp:
@@ -42,28 +37,28 @@ def chooserSave():  # save
 
 
 def loadTrain(model):
-    model.dataset = cargarTextosTraining(model.dataset, chooserDir())
+    model.dataset = ModeloDesp.cargarTextosTraining(model.dataset, chooserDir())
 
 
 def loadTest(model):
-    model.test_set = cargarTextosTest(model.test_set, chooserDir())
+    model.test_set = ModeloDesp.cargarTextosTest(model.test_set, chooserDir())
 
 
 def trainModel(model):
-    model.count_vectorizer, model.selectedModel = model_training(
+    model.count_vectorizer, model.selectedModel = ModeloDesp.model_training(
         model.dataset, "AUTO", 0.06)
 
 
 def saveModel(model):
-    save_model(chooserSave(), model.count_vectorizer, model.selectedModel)
+    ModeloDesp.save_model(chooserSave(), model.count_vectorizer, model.selectedModel)
 
 
 def loadModel(model):
-    model.count_vectorizer, model.selectedModel = load_model(chooserLoad())
+    model.count_vectorizer, model.selectedModel = ModeloDesp.load_model(chooserLoad())
 
 
 def testModel(model):
-    model_testing(model.test_set, model.count_vectorizer, model.selectedModel)
+    ModeloDesp.model_testing(model.test_set, model.count_vectorizer, model.selectedModel)
 
 
 def getNames(dir):
