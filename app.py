e716fdd3aecc2527371c774b8507ecb4f58d2e55
RoachLok <jtabordajunior@hotmail.com>
2021-01-18 05:54:21 +0100

    API route for text uploading and file content querying.

--- app.py
+++ app.py
@@ -15,13 +15,15 @@ def about():
 def get_train_file(file_type):
     if request.method == 'POST':
         json_array = request.get_json()
+        on_file_upload(json_array, file_type)
         return render_template('_dropdowns.html', file_names = json_array, file_type = file_type)
 
-    return render_template('_dropdowns.html', file_type = file_type)
+    file_attri = file_type.split('-', 1)
+    return get_file_content(file_attri[1], file_attri[0])
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
